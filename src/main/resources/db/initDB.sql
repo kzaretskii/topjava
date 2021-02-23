ed57534917cb3588576b4bb6f4e1e9370979579d
kzaretskii <sonto@mail.ru>
2021-02-23 17:49:17 +0300

    HW3 update sql for meals and add JdbcMealRepository

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS user_roles;
+DROP TABLE IF EXISTS meals;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
 
@@ -23,3 +24,16 @@ CREATE TABLE user_roles
     CONSTRAINT user_roles_idx UNIQUE (user_id, role),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+
+CREATE TABLE meals
+(
+    id          INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+    user_id     INTEGER   NOT NULL,
+    dateTime    TIMESTAMP NOT NULL,
+    description VARCHAR   NOT NULL,
+    calories    INTEGER   NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE INDEX meals_user_id_index ON meals (user_id);
+CREATE INDEX meals_dateTime_index ON meals (dateTime);
